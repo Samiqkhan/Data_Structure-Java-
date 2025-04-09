2d3d35f9adea8690a2c057caf0d1a6dc46ca3bc1
Mohammed Samiq Khan <145823604+Samiqkhan@users.noreply.github.com>
2025-04-09 09:55:24 +0530

    Create Vowel_or_Consonant.java

---
+++ Vowel_or_Consonant.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if(ch=='A' || ch =='a' || ch == 'E' || ch == 'e' || ch=='I' || ch=='i' || ch=='O' || ch =='o' || ch == 'U' || ch == 'u'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
